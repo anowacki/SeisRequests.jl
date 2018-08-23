f17981d3d2f209bc2e2cdec00ae68eae2abd5972
Andy Nowacki <a.nowacki@leeds.ac.uk>
2018-08-23 14:16:16 +0100

    Fix documentation of FDSNEvent
    The old docstring contained the wrong parameters; replace
    that with the correct set.
    
    Also tweak formatting of the other FDSNWS docstrings.

--- src/fdsnws.jl
+++ src/fdsnws.jl
@@ -22,18 +22,33 @@ the FDSN Web Services specification.
 
 |Parameter|Description|
 |:--------|:----------|
-|`starttime`|Limit results to time series samples on or after the specified start time|
-|`endtime`|Limit results to time series samples on or before the specified end time|
-|`network`|Select one or more network codes.  Can be SEED network codes or data center defined codes.  Multiple codes are comma-separated.|
-|`station`|Select one or more SEED station codes.  Multiple codes are comma-separated.|
-|`location`|Select one or more SEED location identifiers.  Multiple identifiers are comma-separated.  As a special case “--“ (two dashes) will be translated to a string of two space characters to match blank location IDs.|
-|`channel`|Select one or more SEED channel codes.  Multiple codes are comma-separated.|
-|`quality`|Select a specific SEED quality indicator, handling is data center dependent.|
-|`minimumlength`|Limit results to continuous data segments of a minimum length specified in seconds.|
-|`longestonly`|Limit results to the longest continuous segment per channel.|
-|`format`|Specify format of result, the default value is ‘miniseed’.|
-|`nodata`|Select status code for “no data”, either ‘204’ (default) or ‘404’.|
-"""
+|`starttime`|Limit to events on or after the specified start time.|
+|`endtime`|Limit to events on or before the specified end time.|
+|`minlatitude`|Limit to events with a latitude larger than or equal to the specified minimum.|
+|`maxlatitude`|Limit to events with a latitude smaller than or equal to the specified maximum.|
+|`minlongitude`|Limit to events with a longitude larger than or equal to the specified minimum.|
+|`maxlongitude`|Limit to events with a longitude smaller than or equal to the specified maximum.|
+|`latitude`|Specify the latitude to be used for a radius search.|
+|`longitude`|Specify the longitude to the used for a radius search.|
+|`minradius`|Limit to events within the specified minimum number of degrees from the geographi point defined by the latitude and longitude parameters.|
+|`maxradius`|Limit to events within the specified maximum number of degrees from the geographic point defined by the latitude and longitude parameters.|
+|`mindepth`|Limit to events with depth more than the specified minimum.|
+|`maxdepth`|Limit to events with depth less than the specified maximum.|
+|`minmagnitude`|Limit to events with a magnitude larger than the specified minimum.|
+|`maxmagnitude`|Limit to events with a magnitude smaller than the specified maximum.|
+|`magnitudetype`|Specify a magnitude type to use for testing the minimum and maximum limits.|
+|`includeallorigins`|Specify if all origins for the event should be included, default is data center dependent but is suggested to be the preferred origin only.|
+|`includeallmagnitudes`|Specify if all magnitudes for the event should be included, default is data center dependent but is suggested to be the preferred magnitude only.|
+|`includearrivals`|Specify if phase arrivals should be included.|
+|`eventid`|Select a specific event by ID; event identifiers are data center specific.|
+|`limit`|Limit the results to the specified number of events.|
+|`offset`|Return results starting at the event count specified, starting at 1.|
+|`orderby`|Order the result by time or magnitude with the following possibilities: `"time"`: order by origin descending time; `"time-asc"`: order by origin ascending time; `"magnitude"`: order by descending magnitude; `"magnitude-asc"`: order by ascending magnitude|
+|`catalog`|Limit to events from a specified catalog|
+|`contributor`|Limit to events contributed by a specified contributor.|
+|`updatedafter`|Limit to events updated after the specified time.|
+|`format`|Specify format of result, either `"xml"` (default) or `"text"`. If this parameter is not specified the service must return QuakeML.|
+|`nodata`|Select status code for “no data”, either `204` (default) or `404`.|"""
 @with_kw struct FDSNEvent <: FDSNRequest
     starttime::MDateTime = missing
     endtime::MDateTime = missing
@@ -115,7 +130,7 @@ julia> FDSNDataSelect(station="ANMO", starttime=)
 |`quality`|Select a specific SEED quality indicator, handling is data center dependent.|
 |`minimumlength`|Limit results to continuous data segments of a minimum length specified in seconds.|
 |`longestonly`|Limit results to the longest continuous segment per channel.|
-|`format`|Specify format of result, the default value is ‘miniseed’.|
+|`format`|Specify format of result, the default value is `"miniseed"`.|
 |`nodata`|Select status code for “no data”, either ‘204’ (default) or ‘404’.|
 """
 @with_kw struct FDSNDataSelect <: FDSNRequest
@@ -181,8 +196,8 @@ the FDSN Web Services specification.
 |`includeavailability`|Specify if results should include information about time series data availability.|
 |`updatedafter`|Limit to metadata updated after specified time; updates are data center specific.|
 |`matchtimeseries`|Limit to metadata where selection criteria matches time series data availability.|
-|`format`|Specify format of result, either ‘xml’ (default) or ‘text’.|
-|`nodata`|Select status code for “no data”, either ‘204’ (default) or ‘404’.|
+|`format`|Specify format of result, either `"xml"` (default) or `"text"`.|
+|`nodata`|Select status code for “no data”, either `204` (default) or `404`.|
 """
 @with_kw struct FDSNStation <: FDSNRequest
     starttime::MDateTime = missing
